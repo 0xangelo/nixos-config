6b81278fcced812cb95c823cb1ac5e6e1dba30a1
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2024-06-30 16:35:18 +0200

    feat: update modules/home/git.nix

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,14 +3,28 @@
   programs.git = {
     enable = true;
     
-    userName = "Frost-Phoenix";
-    userEmail = "67cyril6767@gmail.com";
+    userName = "unmaykr (aftermath)";
+    userEmail = "98741738+unmaykr-aftermath@users.noreply.github.com";
     
+    signing = {
+      key = null;
+      signByDefault = true;
+    };
+
     extraConfig = { 
       init.defaultBranch = "main";
       credential.helper = "store";
     };
   };
 
+  programs.gh = {
+    enable = true;
+    # extensions = with pkgs; [gh-markdown-preview];
+    settings = {
+      version = "1";
+      git_protocol = "ssh";
+      prompt = "enabled";
+    };
+  };
   # home.packages = [ pkgs.gh pkgs.git-lfs ];
 }
