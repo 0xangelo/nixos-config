f3010d70fabe56ae5163ca1de93bbec2fa438fbd
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2024-07-03 21:11:02 +0200

    fix PASSWORD_STORE_DIR

--- modules/home/password_store.nix
+++ modules/home/password_store.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, xdg, ... }:
 
 {
   home.packages = (with pkgs; [
@@ -9,7 +9,7 @@
     enable = true;
     package = pkgs.pass-wayland;
     settings = {
-      PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store";
+      PASSWORD_STORE_DIR = "${xdg.dataHome}/password-store";
       PASSWORD_STORE_KEY = "9B4D34AE0C0D3B8F";
       PASSWORD_STORE_CLIP_TIME = "45";
     };
