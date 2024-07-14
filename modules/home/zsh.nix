4a5bf8c052b0088aa0c5ff91c3035b1a2769b6f4
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2024-07-14 13:43:10 +0700

    try adding back zsh-vi-mode plugin

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -1,13 +1,20 @@
-{ host, ...}:
+{ host, pkgs, ...}:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    plugins = [
+      {
+        name = "vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      }
+    ];
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "vi-mode" "fzf"];
+      plugins = [ "git" "fzf" ];
     };
     initExtraFirst = ''
       DISABLE_MAGIC_FUNCTIONS=true
