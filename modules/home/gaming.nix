ef9973d785f1279cdfdbc3753a0a5898bb6671e1
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2024-10-22 11:37:11 -0300

    refactor(gaming): cleanup

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -2,19 +2,7 @@
 {
   home.packages = with pkgs;[
     ## Utils
-    # gamemode
-    # gamescope
     protonplus
     protontricks
-    winetricks
-    # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
-
-    ## Minecraft
-    # prismlauncher
-
-    ## Cli games
-    # _2048-in-terminal
-    # vitetris
-    # nethack
   ];
 }
