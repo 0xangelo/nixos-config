ffced09822e4489b5a5b47ce0bd3ce87ed673882
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2024-12-12 14:26:19 -0300

    chore(networking): add iptables for docker communication with host
    https://discourse.nixos.org/t/docker-container-not-resolving-to-host/30259/8

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -12,6 +12,10 @@
         # { from = 4000; to = 4007; }
         # { from = 8000; to = 8010; }
       # ];
+      extraCommands = ''
+        iptables -I INPUT 1 -s 172.16.0.0/12 -p tcp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 2 -s 172.16.0.0/12 -p udp -d 172.17.0.1 -j ACCEPT
+      '';
     };
   };
 
