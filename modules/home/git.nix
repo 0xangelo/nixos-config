4bfb42d56801e6e728780054ad08172fe7719d31
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-02-13 11:36:28 -0300

    chore(git): invert overrides
    Cargo is having trouble importing ssh deps

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,24 +1,25 @@
 { pkgs, ... }: let
-  workconfig = {
+  personalCfg = {
     user = {
-      name = "unmaykr (aftermath)";
-      email = "98741738+unmaykr-aftermath@users.noreply.github.com";
+      name = "Angelo Gregorio Lovatto (GitHub)";
+      email = "12701614+0xangelo@users.noreply.github.com";
 	  };
 
 	  core = {
-      sshCommand = "ssh -i ~/.ssh/id_ed25519";
+      sshCommand = "ssh -i ~/.ssh/0xangelo_ed25519";
     };
   };
-in {
+in
+{
   programs.git = {
     enable = true;
     
-    userName = "Angelo Gregorio Lovatto (GitHub)";
-	  userEmail = "12701614+0xangelo@users.noreply.github.com";
 
 	  aliases = { 
 	    blame-deep = "blame -w -C -C -C";
 	  };
+    userName = "unmaykr (aftermath)";
+    userEmail = "98741738+unmaykr-aftermath@users.noreply.github.com";
     
     signing = {
       key = null;
@@ -29,14 +30,13 @@ in {
       init.defaultBranch = "main";
       # credential.helper = "store";
       core = {
-	      sshCommand = "ssh -i ~/.ssh/0xangelo_ed25519";
+        sshCommand = "ssh -i ~/.ssh/id_ed25519";
       };
 
       pretty = {
         # https://stackoverflow.com/a/33807182/7842251
         PR = "format:- %h **%s**%n%w(0,2,2)%b";
       };
-
       rerere.enabled = true;
 
       # https://youtu.be/aolI_Rz0ZqY?si=dPdJrGRHBiXvVwui
@@ -49,16 +49,12 @@ in {
     # https://seansantry.com/development/2022/12/14/split-git-nix/
     includes = [
       {
-        condition = "gitdir:AftermathFinance/";
-        contents = workconfig;
+        condition = "gitdir:~/git/0xangelo/";
+        contents = personalCfg;
       }
       {
-        condition = "gitdir:*-aftermath/";
-        contents = workconfig;
-      }
-      {
-        condition = "gitdir:nvim/";
-        contents = workconfig;
+        condition = "gitdir:~/nixos-config/";
+        contents = personalCfg;
       }
     ];
   };
