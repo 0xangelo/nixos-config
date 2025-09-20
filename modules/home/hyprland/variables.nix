f8e907db494586f977e46d180399824675065773
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-09-20 20:10:03 -0300

    chore(hyperland): remove ANKI_WAYLAND from env

--- modules/home/hyprland/variables.nix
+++ modules/home/hyprland/variables.nix
@@ -12,7 +12,6 @@
     SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
     DISABLE_QT5_COMPAT = "0";
     GDK_BACKEND = "wayland";
-    ANKI_WAYLAND = "1";
     WLR_DRM_NO_ATOMIC = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
