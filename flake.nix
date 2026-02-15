ab39af3991963a3ec6645eb8432b2e2fad41b77d
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-15 17:45:51 -0500

    chore: remove catppuccin-cava

--- flake.nix
+++ flake.nix
@@ -3,16 +3,12 @@
 
   inputs = {
     sops-nix.url = "github:Mic92/sops-nix";
-
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
-
     hypr-contrib.url = "github:hyprwm/contrib";
     hyprpicker.url = "github:hyprwm/hyprpicker";
-
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-
     nix-gaming.url = "github:fufexan/nix-gaming";
 
     hyprland = {
@@ -28,10 +24,6 @@
 
     catppuccin-bat = {
       url = "github:catppuccin/bat";
-      flake = false;
-    };
-    catppuccin-cava = {
-      url = "github:catppuccin/cava";
       flake = false;
     };
     catppuccin-starship = {
