acdd5466058fc0cea2697fecaa24b6d2b1af74fd
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-15 20:53:06 -0500

    scripts(maxfetch): remove

--- modules/home/scripts/scripts.nix
+++ modules/home/scripts/scripts.nix
@@ -7,8 +7,6 @@
   toggle_blur = pkgs.writeScriptBin "toggle_blur" (builtins.readFile ./scripts/toggle_blur.sh);
   toggle_oppacity = pkgs.writeScriptBin "toggle_oppacity" (builtins.readFile ./scripts/toggle_oppacity.sh);
 
-  maxfetch = pkgs.writeScriptBin "maxfetch" (builtins.readFile ./scripts/maxfetch.sh);
-
   compress = pkgs.writeScriptBin "compress" (builtins.readFile ./scripts/compress.sh);
   extract = pkgs.writeScriptBin "extract" (builtins.readFile ./scripts/extract.sh);
 
@@ -36,8 +34,6 @@ in {
 
     toggle_blur
     toggle_oppacity
-
-    maxfetch
 
     compress
     extract
