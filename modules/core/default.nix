a3cd3b8f848f6d40362517e40d9152e10c77623b
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-21 16:20:49 -0500

    chore(core): disable nix-ld

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,19 +1,21 @@
-{...}: {
-  imports =
-    [(import ./bootloader.nix)]
-    ++ [(import ./docker.nix)]
-    ++ [(import ./hardware.nix)]
-    # ++ [(import ./keyd.nix)]
-    ++ [(import ./nix-ld.nix)]
-    ++ [(import ./xserver.nix)]
-    ++ [(import ./network.nix)]
-    ++ [(import ./pipewire.nix)]
-    ++ [(import ./postgres.nix)]
-    ++ [(import ./program.nix)]
-    ++ [(import ./security.nix)]
-    ++ [(import ./services.nix)]
-    ++ [(import ./system.nix)]
-    ++ [(import ./user.nix)]
-    ++ [(import ./wayland.nix)]
-    ++ [(import ./virtualization.nix)];
+{ ... }:
+{
+  imports = [
+    (import ./bootloader.nix)
+  ]
+  ++ [ (import ./docker.nix) ]
+  ++ [ (import ./hardware.nix) ]
+  # ++ [(import ./keyd.nix)]
+  # ++ [(import ./nix-ld.nix)]
+  ++ [ (import ./xserver.nix) ]
+  ++ [ (import ./network.nix) ]
+  ++ [ (import ./pipewire.nix) ]
+  ++ [ (import ./postgres.nix) ]
+  ++ [ (import ./program.nix) ]
+  ++ [ (import ./security.nix) ]
+  ++ [ (import ./services.nix) ]
+  ++ [ (import ./system.nix) ]
+  ++ [ (import ./user.nix) ]
+  ++ [ (import ./wayland.nix) ]
+  ++ [ (import ./virtualization.nix) ];
 }
