ca272c9cf8509fbb4776129f1a20cc2b6f3dfc6e
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-24 16:43:45 -0500

    chore(hyprland): remove explicit default misc options

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -41,9 +41,6 @@
       misc = {
         disable_autoreload = true;
         disable_hyprland_logo = true;
-        always_follow_on_dnd = true;
-        layers_hog_keyboard_focus = true;
-        animate_manual_resizes = false;
         enable_swallow = true;
         focus_on_activate = true;
       };
