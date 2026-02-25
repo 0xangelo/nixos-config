d146773fcdbe72c590bff69f4dc597a56250c91f
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-24 20:36:41 -0500

    refactor(starship): use minimal config

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -1,27 +1,6 @@
 _: {
   programs.starship = {
     enable = true;
-
-    enableBashIntegration = true;
     enableZshIntegration = true;
-
-    settings = {
-      directory = {
-        format = "[ ](bold)[ $path ]($style)";
-        style = "bold";
-      };
-
-      character = {
-        success_symbol = "[ ](bold)[ ➜](bold green)";
-        error_symbol = "[ ](bold)[ ➜](bold red)";
-        # error_symbol = "[ ](bold)[ ✗](bold red)";
-      };
-
-      cmd_duration = {
-        format = "[]($style)[[󰔚 ](bg:#161821 fg:#d4c097 bold)$duration](bg:#161821 fg:#BBC3DF)[ ]($style)";
-        disabled = false;
-        style = "bg:none fg:#161821";
-      };
-    };
   };
 }
