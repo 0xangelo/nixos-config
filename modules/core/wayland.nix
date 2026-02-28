4a8378c7ab3ca0f781e6a6bfc98192b02702033f
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-28 09:27:12 -0500

    fix(core/wayland): simplify hyprland setup

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -1,12 +1,3 @@
-{pkgs, ...}: {
+_: {
   programs.hyprland.enable = true;
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    xdgOpenUsePortal = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
 }
