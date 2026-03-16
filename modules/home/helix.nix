039a0c0459757802af637bd254ef2d05aa347faa
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-03-16 09:21:31 -0500

    feat(helix): try using custom tree-sitter-move

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -30,6 +30,41 @@
           "tracing::instrument::WithSubscriber"
         ];
       };
+
+      # [[language]]
+      # name = "move"
+      # scope = "source.move"
+      # injection-regex = "move"
+      # roots = ["Move.toml"]
+      # file-types = ["move"]
+      # comment-token = "//"
+      # indent = { tab-width = 4, unit = "    " }
+      # language-servers = []
+      #
+      # [[grammar]]
+      # name = "move"
+      # source = { git = "https://github.com/tzakian/tree-sitter-move", rev = "8bc0d1692caa8763fef54d48068238d9bf3c0264" }
+
+      # language = [{
+      #   name = "move";
+      #   scope = "source.move";
+      #   injection-regex = "move";
+      #   roots = ["Move.toml"];
+      #   file-types = ["move"];
+      #   comment-token = "//";
+      #   indent = { tab-width = 4; unit = "    "; };
+      #   language-servers = [];
+      # }];
+
+      grammar = [
+        {
+          name = "move";
+          source = {
+            git = "https://github.com/0xangelo/tree-sitter-move";
+            rev = "f7a40b1fdd588311b575cbc4d40291c0a5062f49";
+          };
+        }
+      ];
     };
 
     settings = {
