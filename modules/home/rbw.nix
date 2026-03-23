3e7af20a398272169385e87134b86cfedafa8075
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-03-23 07:33:41 -0500

    refactor(rbw): use lib.getExe

--- modules/home/rbw.nix
+++ modules/home/rbw.nix
@@ -1,9 +1,11 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
   cfg-path = "rbw/config.json";
+  pinentry = lib.getExe pkgs.pinentry-gnome3;
 in {
   programs.rbw.enable = true;
 
@@ -20,13 +22,14 @@ in {
         "notifications_url": null,
         "lock_timeout": 3600,
         "sync_interval": 3600,
-        "pinentry": "${pkgs.pinentry-gnome3}/bin/pinentry",
+        "pinentry": "${pinentry}",
         "client_cert_path": null
       }
     '';
     # path = "${config.xdg.configHome}/${cfg-path}";
   };
 
-  xdg.configFile.${cfg-path}.source = config.lib.file.mkOutOfStoreSymlink "${config.sops.templates.${cfg-path}.path}";
+  xdg.configFile.${cfg-path}.source =
+    config.lib.file.mkOutOfStoreSymlink "${config.sops.templates.${cfg-path}.path}";
   # xdg.configFile.${cfg-path}.source = "${config.sops.templates.${cfg-path}.path}";
 }
