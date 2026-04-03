a232a11bb3e3d2f1198308fa702aa50a9f7ae201
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-04-03 13:35:58 -0500

    refactor(core/networking): simplify

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -2,16 +2,6 @@
   networking = {
     hostName = "dusk";
     networkmanager.enable = true;
-    nameservers = ["1.1.1.1"];
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [22 80 443 59010 59011];
-      allowedUDPPorts = [59010 59011];
-      extraCommands = ''
-        iptables -I INPUT 1 -s 172.16.0.0/12 -p tcp -d 172.17.0.1 -j ACCEPT
-        iptables -I INPUT 2 -s 172.16.0.0/12 -p udp -d 172.17.0.1 -j ACCEPT
-      '';
-    };
   };
 
   environment.systemPackages = with pkgs; [
