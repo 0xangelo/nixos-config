2f26cf79c584c67547692df74dffd6b72f0f028e
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-04-03 13:45:41 -0500

    refactor(zsh): remove Nixos aliases

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -30,14 +30,6 @@
       ll = "eza --icons  -a --group-directories-first -1 --no-user --long";
       tree = "eza --icons --tree --group-directories-first";
 
-      # Nixos
-      ns = "nix-shell --run zsh";
-      nix-shell = "nix-shell --run zsh";
-      nix-switch = "sudo nixos-rebuild switch --flake ~/nixos-config#${host}";
-      nix-switchu = "sudo nixos-rebuild switch --upgrade --flake ~/nixos-config#${host}";
-      nix-flake-update = "sudo nix flake update ~/nixos-config#";
-      nix-clean = "sudo nix-collect-garbage && sudo nix-collect-garbage -d && sudo rm /nix/var/nix/gcroots/auto/* && nix-collect-garbage && nix-collect-garbage -d";
-
       # Git
       ga = "git add";
       gaa = "git add --all";
