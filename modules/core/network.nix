41bd1750c9239ff253e331665c5d40a4c971b26b
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-04-03 13:31:18 -0500

    chore(core/network): remove commented code

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -7,10 +7,6 @@
       enable = true;
       allowedTCPPorts = [22 80 443 59010 59011];
       allowedUDPPorts = [59010 59011];
-      # allowedUDPPortRanges = [
-      # { from = 4000; to = 4007; }
-      # { from = 8000; to = 8010; }
-      # ];
       extraCommands = ''
         iptables -I INPUT 1 -s 172.16.0.0/12 -p tcp -d 172.17.0.1 -j ACCEPT
         iptables -I INPUT 2 -s 172.16.0.0/12 -p udp -d 172.17.0.1 -j ACCEPT
