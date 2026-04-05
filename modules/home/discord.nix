7451650d7dc66b8a3428cfee6e80cfd35f4a4a93
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-04-05 16:16:19 -0500

    style(home/discord): prettify

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,6 +1,4 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [discord];
-  programs.zsh.shellAliases = {
-    discord = "discord --enable-features=UseOzonePlatform --ozone-platform=wayland";
-  };
+  home.packages = [pkgs.discord];
+  programs.zsh.shellAliases.discord = "discord --enable-features=UseOzonePlatform --ozone-platform=wayland";
 }
