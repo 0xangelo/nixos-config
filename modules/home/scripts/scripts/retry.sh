4ec64f9d4672e3d8542e4b920135b8d05666b4ea
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-04-05 21:00:15 -0500

    refactor(scripts/retry): shell checks

--- modules/home/scripts/scripts/retry.sh
+++ modules/home/scripts/scripts/retry.sh
@@ -18,9 +18,9 @@ function retry {
   local count=0
   until "$@"; do
     exit=$?
-    wait=$((2 ** $count))
-    count=$(($count + 1))
-    if [ $count -lt $retries ]; then
+    wait=$((2 ** count))
+    count=$((count + 1))
+    if [ $count -lt "$retries" ]; then
       echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
       sleep $wait
     else
